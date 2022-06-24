9d447dfed11b1c2b04a343a99ca79a1c1555ead1
Iransitumorang <56375877+Iransitumorang@users.noreply.github.com>
2022-06-24 19:16:45 +0700

    Update task1.js

--- Javascript1/task1.js
+++ Javascript1/task1.js
@@ -3,12 +3,26 @@ const biodata = {
     age: 26,
     hobbi : ["Badminton", "Catur", "Swimming"],
     isMerried : false,
-    schoolList : {
-        name : "Pamulang University",
-        yearIn : 2013,
-        yearOut : 2017,
-        major : "Management Economics"
-    },
+    schoolList : [{
+            name: "Pamulang University",
+            yearIn: 2013,
+            yearOut: 2017,
+            major: "Management Economics"
+        },
+        {
+            name: "SMAN1 Bandhar Khalifah",
+            yearIn: 2010,
+            yearOut: 2013,
+            major: "Management Economics"
+
+        },
+        {
+            name: "SMPN1 Bandhar Khalifah",
+            yearIn: 2010,
+            yearOut: 2007,
+            major: "Management Economics"
+        }
+    ],
     skills : {
         html : "Begginer Level",
         css : "Beginner Level",
@@ -17,6 +31,6 @@ const biodata = {
         ReactJs : "Beginner Level"
     },
     interestInCoding : true
-}
+};
 
 console.log(biodata);
